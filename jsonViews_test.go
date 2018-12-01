37c53a1af9def2bc30b0a79fd339c29ca3b081f5
blobdon <zachj1@gmail.com>
2018-12-01 19:03:18 +0000

    jsonViews test builds test json, but still needs to check got != want

--- jsonViews_test.go
+++ jsonViews_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -27,4 +28,30 @@ func TestBookToBookView(t *testing.T) {
 		t.Errorf("Error unmarshalling json to bookView: %s", err)
 	}
 	fmt.Printf("Book: %v \n BookView: %v \n", b, bv)
+}
+
+func TestBuildJSONViews(t *testing.T) {
+	// clean test directory
+	dir := "/Users/blobdon/blurbdata/test/views/"
+	err := os.RemoveAll(dir)
+	if err != nil {
+		t.Fatalf("Failed to clear test views directory: %s", err)
+	}
+	for _, d := range [2]string{"books", "authors"} {
+		err = os.MkdirAll(dir+d, 0755)
+		if err != nil {
+			t.Fatalf("Failed to recreate test views directory for %s: %s", d, err)
+		}
+	}
+
+	db, err := initBolt("/Users/blobdon/blurbdata/test/test.db")
+	if err != nil {
+		t.Errorf("Error on init bolt db: %s", err)
+	}
+	defer db.Close()
+	err = buildJSONViews(db, dir)
+	if err != nil {
+		t.Errorf("Error within buildJSONViews: %s", err)
+	}
+	// add want/get comparisons to check output
 }
